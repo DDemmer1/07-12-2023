3b4d3018037c7cd5818007a9df5937feefc41caf
Unknown <Unknown@DESKTOP-4KSK0J5.fritz.box>
2023-12-07 17:42:16 +0100

    # WARNING: head commit changed in the meantime
    Merge branch 'main' of https://github.com/DDemmer1/07-12-2023 into main

--- src/de/demmer/dennis/Wiederholung.java
+++ src/de/demmer/dennis/Wiederholung.java
@@ -2,16 +2,38 @@ package de.demmer.dennis;
 
 public class Wiederholung {
 	
-	public static void main(String[] args) {
-		
-		
-		
-		
-		
-		
-		
+	static void testMethode() {
+		System.out.println("Testmethode");
 	}
 	
 	
-	
+
+	public static void main(String[] args) {
+
+		int i = 5;
+
+		int y = i + 2;
+
+		String test = "Test String";
+		
+		testMethode();
+
+		System.out.println(test);
+		
+		//        0       1      2        3       4
+		//   |   'T'  |       |       |       |        |
+		char[] cArr = new char[5];
+		
+		cArr[0] = 'T';
+		System.out.println(cArr[0]);
+		
+		System.out.println(cArr.length);
+		
+		//for -> STRG + Leertaste -> 2. Option
+		for (int j = 0; j < cArr.length; j++) {
+			
+		}
+
+	}
+
 }
